648abb94bb86bf84d42c9f57a4bb7f52489f00ad
czhcheng27 <czhcheng27@gmail.com>
2020-12-20 22:21:47 -0500

    admin sub-router

---
+++ react_manager_client/src/components/left-nav/left-nav.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react' 
+
+import './left-nav.less'
+
+export default class LeftNav extends Component { 
+    render () { 
+        return ( 
+            <div className='left-nav'>LeftNav</div>
+        )
+    }
+}
--- react_manager_client/src/pages/admin/admin.jsx
+++ react_manager_client/src/pages/admin/admin.jsx
@@ -1,26 +1,49 @@
-import React, {Component} from 'react' 
-import {Button} from 'antd'
-import {connect} from 'react-redux'
-import { Redirect } from 'react-router-dom';
+import React, { Component } from 'react'
+import { Layout } from 'antd'
+import { connect } from 'react-redux'
+import { Redirect, Route, Switch } from 'react-router-dom';
 
+import LeftNav from '../../components/left-nav/left-nav'
+import Head from '../../components/head/head'
+import Home from '../home/home'
+import Team from '../team/team'
+import Player from '../player/player'
+import User from '../user/user'
+import Role from '../role/role'
 
-class Admin extends Component { 
-    render () { 
+const {  Footer, Sider, Content } = Layout;
+
+class Admin extends Component {
+    render() {
 
         const user = this.props.user
-        if(!user || !user._id){
+        if (!user || !user._id) {
             return <Redirect to='/login' />
         }
 
-        return ( 
-            <div>
-                Hello {user.username}
-            </div>
+        return (
+            <Layout style={{ minHeight: '100%' }}>
+                <Sider><LeftNav /></Sider>
+                <Layout>
+                    <Head>Head</Head>
+                    <Content style={{ backgroundColor: '#fff' }}>
+                        <Switch>
+                            <Redirect exact from='/' to='/home' />
+                            <Route path='/home' component={Home} />
+                            <Route path='/team' component={Team} />
+                            <Route path='/player' component={Player} />
+                            <Route path='/user' component={User} />
+                            <Route path='/role' component={Role} />
+                        </Switch>
+                    </Content>
+                    <Footer style={{textAlign: 'center'}}>Develop by Zihang Cheng</Footer>
+                </Layout>
+            </Layout>
         )
     }
 }
 
 export default connect(
-    state => ({user: state.user}),
+    state => ({ user: state.user }),
     {}
 )(Admin)
