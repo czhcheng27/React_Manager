c5a80b0734df5a25c2cadf22eb6a9d9a94e38a8a
czhcheng27 <63081580+czhcheng27@users.noreply.github.com>
2021-03-02 10:02:40 -0500

    Delete dateUtlis.js

--- dateUtlis.js
+++
@@ -1,5 +0,0 @@
-export default function formDate(time) {
-    let date = new Date(time)
-    return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate() + 
-    ' ' + date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds()
-}
